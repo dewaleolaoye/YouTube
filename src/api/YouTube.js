ce58aae23988334e88e871b3d5d874f99cc79511
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-09-24 21:04:31 +0100

    hide secret key

--- src/api/YouTube.js
+++ src/api/YouTube.js
@@ -1,15 +1,10 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-dotenv.config();
-const KEY = '';
-
-console.log(process.env.REACT_APP_KEY);
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
     maxResults: 5,
-    key: KEY
+    key: process.env.REACT_APP_KEY
   }
 });
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoItem = ({ video }) => {
   return (
     <div>
-      <img src={video.snippet.thumbnails.medium.url} />
+      <img src={video.snippet.thumbnails.medium.url} alt="videos" />
       {video.snippet.title}
     </div>
   );
