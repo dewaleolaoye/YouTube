0c5ea48424ab6c30d99644b06c3c02f7a0e241ff
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-09-25 11:06:33 +0100

    fix default loaded video

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,21 @@ import VideoDetail from '../components/VideoDetail';
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('Grit');
+  }
+
   onTermSubmit = async term => {
     const response = await Youtube.get('/search', {
       params: {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
